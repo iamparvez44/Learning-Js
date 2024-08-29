77c9b25e198d286bc73268edb7b398c02b677fb4
iamparvez44 <parvez.alam44@gmail.com>
2024-08-30 01:08:46 +0530

    js_basics

---
+++ intro.js
@@ -0,0 +1,63 @@
+// let a = 45;
+
+
+// let b = "hey there i am a string";
+
+// let c = true;
+// let d =  null;
+
+// console.log(a, b, c, d);
+
+// chapter 01 practice set
+
+// here i am adding two data types.
+
+// let a =  ("Parvez");
+// let b = 25;
+
+// console.log(a + b);
+// console.log(typeof b);
+
+// const a = 57;
+
+// console.log(a);
+
+// const a = {
+//     name: "Parvez",
+//     section: 1,
+//     isPrinciple: false
+// }
+// a['freiend'] = "Haris"
+
+// console.log(a);
+
+// Q4 - make a dicsnory
+
+// const dics = {
+//     erudite : "characterized by great knowledge; learned or scholarly",
+//     tomfoolery : "foolish or silly behavior",
+//     nephogram : "a picture of a cloud or clouds",
+//     isingenuous : "lacking in frankness, candor, or sincerity",
+// }
+
+// console.log(dics.isingenuous);
+
+// Operators in Js
+
+// let a = 3;
+// let b = (100+50);
+
+// let result = a*b;
+
+// console.log(result);
+
+//assignment operator
+
+let a = 10;
+console.log(a);
+a+=5;
+console.log(a);
+a-=3;
+console.log(a);
+a*=5;
+console.log(a);
